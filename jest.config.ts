6a782f7995b8bfd25209367f28adaf6fb8def5c3
ivanjermakov <ivanjermakov1@gmail.com>
2023-05-05 00:56:11 +0200

    Speed up jest tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
     moduleDirectories: ['node_modules', '<rootDir>'],
-    preset: 'ts-jest',
     testEnvironment: 'node',
+    transform: {
+        '^.+\\.(t|j)sx?$': '@swc/jest',
+    },
 }
