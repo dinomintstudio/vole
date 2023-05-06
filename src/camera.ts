baeba65702bfa976f1d8c06416a8af2d8f0917cb
ivanjermakov <ivanjermakov1@gmail.com>
2023-05-06 20:36:35 +0200

    Camera

---
+++ src/camera.ts
@@ -0,0 +1,17 @@
+import {Vector} from './vector'
+
+export class Camera {
+
+    pos: Vector
+    zoom: number
+
+    constructor(pos: Vector, zoom: number) {
+        this.pos = pos
+        this.zoom = zoom
+    }
+
+    static defalut(): Camera {
+        return new Camera(Vector.Zero, 1)
+    }
+
+}
