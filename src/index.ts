e0a1d3987b800a89ed210c69ad4209a78976ee83
ivanjermakov <ivanjermakov1@gmail.com>
2023-11-18 22:53:53 +0100

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,7 @@ export * from './engine'
 export * from './entity'
 export * from './scene'
 
-export * from './eventable'
-
-export * from './component'
-
 export * from './camera'
-
-export * from './input'
 
 export * from './vector'
 export * from './color'
