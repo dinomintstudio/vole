498b80e87e7dee0b852abb07e3f39afa80a5f37b
ivanjermakov <ivanjermakov1@gmail.com>
2023-12-06 19:56:45 +0100

    Draw component visibility

--- src/component/canvas-draw.component.ts
+++ src/component/canvas-draw.component.ts
@@ -4,10 +4,17 @@ import { Component } from './component'
 export class CanvasDrawComponent extends Component {
 
     draw: (ctx: CanvasRenderingContext2D) => boolean
+    visible: boolean
 
-    constructor(entity: Entity, draw: (ctx: CanvasRenderingContext2D) => boolean) {
+    constructor(
+        public entity: Entity,
+        draw: (ctx: CanvasRenderingContext2D) => boolean,
+        visible: boolean = true,
+    ) {
         super(entity)
         this.draw = draw
+        this.visible = visible
     }
+
 }
 
